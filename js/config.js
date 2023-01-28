f52be8af15079c9eb2f2d288bf01edab245ae5ab
Florence Leclercq <florence.leclercq.888@outlook.fr>
2023-01-28 12:18:29 +0100

    Modification de l'adresse Url dans config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-// let apiUrl = (location.hostname==="localhost" || location.hostname==="127.0.0.1")? "http://localhost:3000" : "https://florence-leclercq.alwaysdata.net/kanap-back";
+// let apiUrl = (location.hostname==="localhost" || location.hostname==="127.0.0.1")? "http://localhost:3000" : "https://kanap-back-flo701.onrender.com";
 
-let apiUrl = "https://florence-leclercq.alwaysdata.net/kanap-back";
+let apiUrl = "https://kanap-back-flo701.onrender.com";
 
 
 
